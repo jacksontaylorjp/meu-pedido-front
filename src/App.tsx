e63af4e7084aea153ddf78290f12b302bbf7bbe0
jacksontaylorjp <jacksontaylorjp@gmail.com>
2024-08-12 21:50:33 -0300

    feat: implementação do privateRouter

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import RecuperarSenha from './pages/RecuperarSenha';
+import PrivateRoute from './componentes/PrivateRoute';
 
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/cadastro" element={<Cadastro/>} />
-        <Route path="/recuperar-senha" element={<RecuperarSenha/>} />
+        <Route path="/" element={<Login />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/home" element={<Home />} />
+        </Route>        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
       </Routes>
     </Router>
   );
